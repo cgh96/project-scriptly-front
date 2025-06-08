9364194ddc4754572c92f25424d6bd8efb8fad71
cgh96 <tt878429@gmail.com>
2025-06-08 16:27:34 +0900

    chore : 프로젝트 초기세팅
    - eslint, prettier
    - 라이브러리 설치 react-router, styled-components 등
    - index.css에 reset css작성
    - README 초안 작성
    - baseURL 설정 및 path alias작성

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // If you want to check it in dev,
+      // add the devOptions option to the plugin configuration
+      // (you will have the Web App Manifest and the generated service worker)
+      // devOptions: {
+      //   enabled: true,
+      // },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@app": "/src/app",
+      "@pages": "/src/pages",
+      "@shared": "/src/shared",
+      "@widgets": "/src/widgets",
+      "@features": "/src/features",
+      "@entities": "/src/entities",
+      "@assets": "/src/assets",
+    },
+  },
+});
