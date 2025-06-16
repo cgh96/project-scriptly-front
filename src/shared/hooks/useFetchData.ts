ec782550fcdedbc61be76181db7306c962750aa4
cgh96 <tt878429@gmail.com>
2025-06-16 16:15:16 +0900

    feat : useFetchData 공통 훅 구현

---
+++ src/shared/hooks/useFetchData.ts
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+interface UseFetchDataOptions {
+  immediate?: boolean; // 즉시 실행할지 여부
+  deps?: React.DependencyList; // 의존성 배열
+}
+
+export const useFetchData = <T>(fetchFn: () => Promise<T>, options?: UseFetchDataOptions) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const { immediate = true, deps = [] } = options ? options : {};
+
+  const executeFetchFn = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFn();
+      setData(result);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : '';
+
+      setError(errorMessage);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }, [fetchFn]);
+
+  const clearError = useCallback(() => {
+    setError(null);
+  }, []);
+
+  useEffect(() => {
+    if (immediate) {
+      executeFetchFn();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [immediate, executeFetchFn, ...deps]);
+
+  return {
+    data,
+    error,
+    loading,
+    refetch: executeFetchFn,
+    setData,
+    clearError,
+  };
+};
