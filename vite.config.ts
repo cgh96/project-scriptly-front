7ecf1017ee4e2e4136c743c197ba8b8cb7e813c3
cgh96 <tt878429@gmail.com>
2025-07-06 13:48:13 +0900

    chore: svg icon을 React Component처럼 사용할 수 있도록 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       // If you want to check it in dev,
       // add the devOptions option to the plugin configuration
       // (you will have the Web App Manifest and the generated service worker)
@@ -18,14 +20,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": "/src",
-      "@app": "/src/app",
-      "@pages": "/src/pages",
-      "@shared": "/src/shared",
-      "@widgets": "/src/widgets",
-      "@features": "/src/features",
-      "@entities": "/src/entities",
-      "@assets": "/src/assets",
+      '@': '/src',
+      '@app': '/src/app',
+      '@pages': '/src/pages',
+      '@shared': '/src/shared',
+      '@widgets': '/src/widgets',
+      '@features': '/src/features',
+      '@entities': '/src/entities',
+      '@assets': '/src/assets',
     },
   },
 });
