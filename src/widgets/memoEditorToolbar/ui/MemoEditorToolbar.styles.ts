203299576d89c81853739615921da531c49d4483
cgh96 <tt878429@gmail.com>
2025-07-12 11:57:27 +0900

    feat: MemoEditorToolbar UI컴포넌트 구현

---
+++ src/widgets/memoEditorToolbar/ui/MemoEditorToolbar.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { spacing } from '@/shared/config/styles';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${spacing.sm};
+`;
---
+++ src/widgets/memoEditorToolbar/ui/MemoEditorToolbar.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router';
+
+import { useCreateMemo } from '@/entities/memo/api';
+import { CreateMemoIconButton } from '@/features/memoCreate/ui';
+
+import * as S from './MemoEditorToolbar.styles';
+
+export const MemoEditorToolbar = () => {
+  const navigate = useNavigate();
+
+  const { mutate } = useCreateMemo({
+    useHttp: false,
+    onSuccess: (data) => {
+      navigate(`/memo/${data.id}`, { replace: true });
+    },
+  });
+
+  const handleCreateMemo = () => {
+    mutate({
+      title: '제목 없음',
+      content: '',
+      isPublic: true,
+    });
+  };
+
+  return (
+    <S.Container>
+      <CreateMemoIconButton onClick={handleCreateMemo} />
+    </S.Container>
+  );
+};
